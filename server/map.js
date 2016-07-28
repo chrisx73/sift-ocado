a80ce95e9d597cd989957c545a261008998187c3
Randal Pinto <randal@redsift.io>
2016-07-28 13:23:52 +0100

    no message

--- server/map.js
+++ server/map.js
@@ -8,17 +8,6 @@ const OrderRegExp = {
   TOTAL: /(Total \(estimated\))\D*(\d+\.\d+)/
 };
 
-// Converts a Date into YYYY<s>MM<s>DD format
-function yyyymmdd(d, separator) {
-  if (!separator) {
-    separator = '';
-  }
-  var yyyy = d.getFullYear().toString();
-  var mm = (d.getMonth() + 1).toString();
-  var dd = d.getDate().toString();
-  return yyyy + separator + (mm[1] ? mm : '0' + mm[0]) + separator + (dd[1] ? dd : '0' + dd[0]);
-}
-
 // Converts a Date into YYYY<s>MM format
 function yyyymm(d, separator) {
   if (!separator) {
@@ -29,17 +18,10 @@ function yyyymm(d, separator) {
   return yyyy + separator + (mm[1] ? mm : '0' + mm[0]);
 }
 
-// You can require dependencies following the node paradigm
-// e.g. var moment = require('moment');
-
 // Entry point for DAG node
 module.exports = function(got) {
   // inData contains the key/value pairs that match the given query
   const inData = got['in'];
-  // The query matched (array of elements based on the store's key and your selection criteria)
-  const query = got['query'];
-  // Joined information from your 'with' statement
-  const withData = got['with'];
 
   console.log('sift-ocado: map.js: running...');
 
@@ -59,7 +41,7 @@ module.exports = function(got) {
           tot = OrderRegExp.TOTAL.exec(msgBody);
         }
         //console.log('MAP: tot: ', tot);
-       
+
         // if found total and managed to extract value from it
         if (tot && tot.length === 3) {
           var total = tot[2];
@@ -69,7 +51,6 @@ module.exports = function(got) {
           var date = new Date(msg.date);
           ret.push({
             name: 'messages',
-            //key: yyyymmdd(date, '-') + '/' + msg.id,
             key: yyyymm(date) + '/' + date.getFullYear().toString() + '/' + msg.id,
             value: {
               total: total,
@@ -89,29 +70,6 @@ module.exports = function(got) {
     }
   }
 
-/*
-  var result = inData.data.map(function(datum){
-    var jmapInfo = JSON.parse(datum.value);
-    console.log('sift-ocado: map.js: inData: key', datum.key);
-    console.log('sift-ocado: map.js: inData: value', jmapInfo);
-
-    // Emit an empty object for each message so count can be calculated in the next node
-    return {
-      name: 'messages',
-      key: datum.key,
-      value: {}
-    };
-  });
-  */
-
-
   console.log('MAP: ret: ', ret);
   return ret;
-  // Possible return values are: undefined, null, promises, single or an array of objects
-  // return objects should have the following structure
-  // {
-  //   name: '<name of node output>',
-  //   key: 'key1',
-  //   value: '1'
-  // };
 };
--- server/reduce.js
+++ server/reduce.js
@@ -1,11 +1,7 @@
 /**
  * Sift Ocado. DAG's 'Reduce' node implementation
  */
-
 'use strict';
-
-// You can require dependencies following the node paradigm
-// e.g. var moment = require('moment');
 
 // Entry point for DAG node
 module.exports = function(got) {
@@ -13,12 +9,9 @@ module.exports = function(got) {
   const inData = got['in'];
   // The query matched (array of elements based on the store's key and your selection criteria)
   const query = got['query'];
-  // Joined information from your 'with' statement
-  const withData = got['with'];
 
   console.log('sift-ocado: reduce.js: running...');
-  
-  //inData.data.map(function(){ orderCount++; });
+
   var total = 0;
   for (var d of inData.data) {
     console.log('REDUCE: key: ', d.key);
@@ -27,18 +20,8 @@ module.exports = function(got) {
     console.log('REDUCE: total: ', tot);
     total = total + tot;
   }
-
   var month = query[0];
   console.log('REDUCE: month: ', month);
 
-  // Return the total count of emails from @gmail.com
   return ({name: 'count', key: month, value: total});
-
-  // Possible return values are: undefined, null, promises, single or an array of objects
-  // return objects should have the following structure
-  // {
-  //   name: '<name of node output>',
-  //   key: 'key1',
-  //   value: '1'
-  // };
 };
